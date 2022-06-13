b4789f5edca5bc22006d81767001eac73222a4c0
Mark Small <mark.small@astrosat.net>
2022-06-13 20:26:13 +0100

    fix(frontend): Some assorted changes
    * Renamed page heading
    * Reformated tailwind config file
    * Changed content of App

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div className="min-h-screen flex justify-center items-center">
-        <h1 className="text-3xl font-bold text-blue-600">
-          Install and Setup Vite, React and Typescript with Tailwind CSS 3
-        </h1>
-      </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>I have a meeting to show this off at 15:00 on Wednesday
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header> */}
-    </div>
+    <main className="min-h-screen flex items-center justify-center text-6xl bg-teal-400 sm:bg-blue-400 md:bg-red-400 lg:bg-green-400 text-teal-100">
+      <h1 className="">
+        Install and Setup Vite, React and Typescript with Tailwind CSS 3
+      </h1>
+    </main>
   );
 }
 
